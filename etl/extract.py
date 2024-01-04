6773d56fc96597e79e1320d180e702be4dbeb20a
Abasifreke Nkanang <nkanangabasifreke.data@gmail.com>
2024-01-04 15:22:43 +0100

    Implemented error handling for api requests, and logging

--- etl/extract.py
+++ etl/extract.py
@@ -3,6 +3,10 @@ import json
 from datetime import datetime, timedelta
 import os
 import yaml
+import logging
+
+# Implement logger
+logging.basicConfig(filename='extract.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s')
 
 # Define function to load last timestamp in config
 def load_last_timestamp():
@@ -28,49 +32,56 @@ lichess_username = api_config['lichess']['user']
 # Define function to fetch data from lichess
 def fetch_lichess_data(username):
     
-    since = load_last_timestamp()
-    until = datetime.now().strftime("%Y%m%d%H%M%S")
-    
-    url = f"{lichess_api_url}/{username}"    
-    headers = {"Accept": "application/x-ndjson"}
-    params = {
-        "since": since,
-        "until": until,
-        "max":2,
-        "perfType": "ultraBullet, bullet, blitz",
-        "analysed": True,
-        "pgnInJson": True,
-        "clocks": True,
-        "evals": True,
-        "opening": True,
-        "sort": "dateAsc"
-    }
-    
-    response = requests.get(url, params=params, headers=headers, stream=True)
-    
-    if response.status_code == 200:
+    try:
+        since = load_last_timestamp()
+        until = datetime.now().strftime("%Y%m%d%H%M%S")
         
-        # Save the current timestamp for future requests
-        save_last_timestamp(until)
+        url = f"{lichess_api_url}/{username}"    
+        headers = {"Accept": "application/x-ndjson"}
+        params = {
+            "since": since,
+            "until": until,
+            "max":2,
+            "perfType": "ultraBullet, bullet, blitz",
+            "analysed": True,
+            "pgnInJson": True,
+            "clocks": True,
+            "evals": True,
+            "opening": True,
+            "sort": "dateAsc"
+        }
+        
+        response = requests.get(url, params=params, headers=headers, stream=True)
+        response.raise_for_status() # Raise an HTTPError for bad responses
+        
+        if response.status_code == 200:
+            save_last_timestamp(until) # Save the current timestamp for future requests
 
-    # Process NDJSON response
-    lichess_data = []
-    for line in response.iter_lines():
-        if line:
-            lichess_data.append(json.loads(line.decode('utf-8')))
+        # Process NDJSON response
+        lichess_data = []
+        for line in response.iter_lines():
+            if line:
+                lichess_data.append(json.loads(line.decode('utf-8')))
 
-    return lichess_data
+        return lichess_data
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to fetch data from Lichess for user {username}. Error: {e}")
+        return None
 
 # Define function to extract data
 def extract_data(username):
     lichess_data = fetch_lichess_data(username)
 
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    output_filename = f"data/raw/{username}_data_{timestamp}.json"
+    if lichess_data is not None:
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        output_filename = f"data/raw/{username}_data_{timestamp}.json"
 
-    with open(output_filename, 'w') as output_file:
-        json.dump({'lichess': lichess_data}, output_file)
+        with open(output_filename, 'w') as output_file:
+            json.dump({'lichess': lichess_data}, output_file)
 
 if __name__ == "__main__":
-    username = lichess_username
-    extract_data(username)
+    try:
+        username = lichess_username
+        extract_data(username)
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
