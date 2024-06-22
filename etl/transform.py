92026b6185d2481156d83eefa2eeadb518d85d6d
Abasifreke Nkanang <nkanangabasifreke.data@gmail.com>
2024-06-22 02:25:33 +0100

    Updated helper and main functions

--- etl/transform.py
+++ etl/transform.py
@@ -1,11 +1,24 @@
-import yaml
-import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode, expr, udf
-from pyspark.sql.types import StructType, StructField, StringType, BooleanType, LongType, IntegerType, ArrayType
+from pyspark.sql.functions import col
+from pathlib import Path
+import os
+import yaml
+import logging
+
+# Define config path and data directory
+config_path = Path("config")
+data_dir = Path("data")
+
+# Implement logger
+logging.basicConfig(
+    filename='extract.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s: %(message)s'
+)
 
 def load_config():
-    with open('config/etl_config.yml', 'r') as config_file:
+    path = config_path / "etl_config.yml"
+    with open(path, 'r') as config_file:
         return yaml.safe_load(config_file)
 
 def load_processed_files():
@@ -16,46 +29,53 @@ def load_processed_files():
         return set()
 
 def save_processed_file(filename):
-    with open('config/processed_files.txt', 'a') as file:
+    path = config_path / "processed_files.txt"
+    with open(path, 'a') as file:
         file.write(f"{filename}\n")
 
-def transform_data(raw_data):
-    # Define UDFs
-    split_moves_udf = udf(lambda moves: moves.split(), ArrayType(StringType()))
-    time_efficiency_udf = udf(lambda initial, total: (total - initial) / total)
-
+def write_to_pgn(games, file, output_path):
     
-    # Transformation logic for each game
-    transform_data = raw_data.select(
-        col("id").alias("game_id"),
-        col("rated"),
-        col("variant"),
-        col("speed"),
-        col("perf"),
-        col("createdAt"),
-        col("lastMoveAt"),
-        col("status"),
-        col("players.white.user.name").alias("white_name"),
-        col("players.white.user.id").alias("white_id"),
-        col("players.white.user.title").alias("white_title"),
-        col("players.white.rating").alias("white_rating"),
-        col("players.white.ratingDiff").alias("white_rating_diff"),
-        col("players.black.user.name").alias("black_name"),
-        col("players.black.user.id").alias("black_id"),
-        col("players.black.rating").alias("black_rating"),
-        col("players.black.ratingDiff").alias("black_rating_diff"),
-        col("winner"),
-        col("opening.eco").alias("opening_eco"),
-        col("opening.name").alias("opening_name"),
-        col("opening.ply").alias("opening_ply"),
-        split_moves_udf(col("moves")).alias("moves"),
-        expr("size(moves)").alias("move_count"),
-        time_efficiency_udf(col("clock.initial"), col("clock.totalTime")).alias("time_efficiency")
-    )
-    return transform_data
+    filename, ext = os.path.splitext(file)
+        
+    with open(f"{output_path}/{filename}.pgn", 'a') as pgn:
+        count = 1
+        for game in games:
+            if count == 1:
+                pgn.write(f'[Game {count}]\n')
+            else:
+                pgn.write(f'\n[Game {count}]\n')
+            pgn.write(f'[Game ID "{game["game_id"]}"]\n')
+            pgn.write(f'[White "{game["white_name"]}"]\n')
+            pgn.write(f'[Black "{game["black_name"]}"]\n')
+            pgn.write(f'[Opening Eco "{game["opening_eco"]}"]\n')
+            pgn.write(f'[Opening Name "{game["opening_name"]}"]\n')
+            pgn.write(f'[Game Winner "{game["winner"]}"]\n')
+            pgn.write(f'\n{game["moves"]}\n')
+            count += 1
+
+
+def parse_game(game_data):
+    game_id = game_data["game_id"]
+    white_name = game_data["white_name"]
+    black_name = game_data["black_name"]
+    opening_eco = game_data["opening_eco"]
+    opening_name = game_data["opening_name"]
+    winner = game_data["winner"]
+    moves_str = game_data["moves"]
+    
+    return {
+        "game_id": game_id,
+        "white_name": white_name,
+        "black_name": black_name,
+        "opening_eco": opening_eco,
+        "opening_name": opening_name,
+        "winner": winner,
+        "moves": moves_str
+    }
 
 def main():
     etl_config = load_config()
+    processed_files = load_processed_files()
 
     # Initialize Spark session
     spark = SparkSession.builder \
@@ -68,116 +88,38 @@ def main():
     # Read raw data
     raw_data_path = etl_config['data']['raw_data_path']
     transformed_data_path = etl_config['data']['transformed_data_path']
-    raw_data = spark.read.json(f"{raw_data_path}/drnykterstein_data_20240114120856.json", multiLine=True)
-    raw_data.show()
+    new_files = [file for file in os.listdir(raw_data_path) if file.endswith(".ndjson") and file not in processed_files]
     
-    split_moves_udf = udf(lambda moves: moves.split(), ArrayType(StringType()))
-    time_efficiency_udf = udf(lambda initial, total: (total - initial) / total)
+    for file in new_files:
+        df = spark.read.json(f"{raw_data_path}/{file}")
+        
+        row_count = df.count()
+        logging.info(f"File: {file} - Number of rows read: {row_count}")
+        
+        # Filter for games that ended in a mate (potential puzzles)
+        filtered_df = df.filter((df.status == "mate") & (df.variant == "standard"))
+        
+        # Process data within Spark
+        processed_games = filtered_df.select(
+            col("id").alias("game_id"),
+            col("players.white.user.name").alias("white_name"),
+            col("players.black.user.name").alias("black_name"),
+            col("opening.eco").alias("opening_eco"),
+            col("opening.name").alias("opening_name"),
+            col("winner"),
+            col("moves"),
+        ).rdd.map(lambda row: parse_game(row.asDict()))
+        
+        processed_games_count = processed_games.count()
+        logging.info(f"File: {file} - Number of processed games: {processed_games_count}")        
+        
+        # Write each game to PGN file within 'foreachPartition'
+        processed_games.foreachPartition(lambda partition: write_to_pgn(partition, file, transformed_data_path))
+            
+        save_processed_file(file)
+
+    spark.stop()
+
     
-    # Apply transformation to each game
-    transformed_data = raw_data.select(
-        col("lichess.id").alias("game_id"),
-        col("lichess.rated"),
-        col("lichess.variant"),
-        col("lichess.speed"),
-        col("lichess.perf"),
-        col("lichess.createdAt"),
-        col("lichess.lastMoveAt"),
-        col("lichess.status"),
-        col("lichess.players.white.user.name").alias("white_name"),
-        col("lichess.players.white.user.id").alias("white_id"),
-        col("lichess.players.white.user.title").alias("white_title"),
-        col("lichess.players.white.rating").alias("white_rating"),
-        col("lichess.players.white.ratingDiff").alias("white_rating_diff"),
-        col("lichess.players.black.user.name").alias("black_name"),
-        col("lichess.players.black.user.id").alias("black_id"),
-        col("lichess.players.black.rating").alias("black_rating"),
-        col("lichess.players.black.ratingDiff").alias("black_rating_diff"),
-        col("lichess.winner"),
-        col("lichess.opening.eco").alias("opening_eco"),
-        col("lichess.opening.name").alias("opening_name"),
-        col("lichess.opening.ply").alias("opening_ply")
-    )
-
-    # If needed, you can perform additional transformations or filtering here
-
-    # Show the transformed data
-    transformed_data.show()
-    
-    """  
-    # Load processed files
-    processed_files = load_processed_files()
-
-    # Get list of new files
-    raw_data_path = etl_config['data']['raw_data_path']
-    transformed_data_path = etl_config['data']['transformed_data_path']
-    new_files = [file for file in os.listdir(raw_data_path) if file.endswith(".json") and file not in processed_files]
-
-    # Define the schema for the JSON data 
-    lichess_schema = StructType([
-        StructField("lichess", ArrayType(
-            StructType([
-                StructField("id", StringType(), True),
-                StructField("rated", BooleanType(), True),
-                StructField("variant", StringType(), True),
-                StructField("speed", StringType(), True),
-                StructField("perf", StringType(), True),
-                StructField("createdAt", LongType(), True),
-                StructField("lastMoveAt", LongType(), True),
-                StructField("status", StringType(), True),
-                StructField("players", StructType({
-                    StructField("white", StructType({
-                        StructField("user", StructType({
-                            StructField("name", StringType()), 
-                            StructField("title", StringType()), 
-                            StructField("flair", StringType()), 
-                            StructField("patron", BooleanType()), 
-                            StructField("id", StringType())
-                        })), 
-                        StructField("rating", IntegerType()), 
-                        StructField("ratingDiff", IntegerType())
-                    })),
-                    StructField("black", StructType({
-                        StructField("user", StructType({
-                            StructField("name", StringType()), 
-                            StructField("title", StringType()), 
-                            StructField("flair", StringType()), 
-                            StructField("patron", BooleanType()), 
-                            StructField("id", StringType())
-                        })), 
-                        StructField("rating", IntegerType()), 
-                        StructField("ratingDiff", IntegerType()),
-                        StructField("provisional", BooleanType())
-                    }))
-                })), 
-                StructField("winner", StringType(), True),
-                StructField("opening", StructType({
-                    StructField("eco", StringType()), 
-                    StructField("name", StringType()), 
-                    StructField("ply", IntegerType())
-                }), True),
-                StructField("moves", StringType(), True),
-                StructField("clocks", ArrayType(LongType()), True),
-                StructField("clock", StructType({
-                    StructField("initial", IntegerType()),
-                    StructField("increment", IntegerType()),
-                    StructField("totalTime", IntegerType())
-                }), True)
-            ])
-        ))
-    ])
-
-    for filename in new_files:
-    """
-
-
-    # Save transformed data
-    filename = "drnykterstein_data_20240117012957.json"
-    transformed_data.write.mode("overwrite").json("output1")
-
-    # Update list of processed files
-    #save_processed_file(filename)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
